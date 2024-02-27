9db64030d4d4ddd1c10b9d1c42f021afbfd3c1f0
Dharshan <136823343+Dharshacr7@users.noreply.github.com>
2024-02-27 12:59:56 +0530

    change

---
+++ index.js
@@ -0,0 +1,6 @@
+const popUpBox = document.getElementById('popupbox');
+const getInTouch = document.getElementById('popUp')
+
+getInTouch.addEventListener('click', ()=> {
+    popUpBox.style.display = "block";
+})
